acd569c3ac540abdd23fb40a917a42629566b24d
Radhika Bhatt <rbhatt@doc.gov>
2016-08-09 11:46:12 -0400

    temporarily disabled scrollspy

--- js/nzertf.js
+++ js/nzertf.js
@@ -14,9 +14,11 @@ $('.navbar-toggle').click(function () {
 });
 
 
-// Scroll Spy for Side Navbar
-$('body').scrollspy({ target: '.sidenav-padding' });
+// // Scroll Spy for Side Navbar
+// $('body').scrollspy({ target: '#side-bar', offset: 90});
 
+// // Refresh Scrollspy
 // $('[data-spy="scroll"]').each(function () {
-//   var $spy = $(this).scrollspy('refresh')
+//   var $spy = $(this).scrollspy('refresh');
 // });
+
