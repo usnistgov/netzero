942454172b620c67652437cad88b04142465c451
Mark Brown II <mbrownii@doc.gov>
2016-10-21 11:03:28 -0400

    Removes href in mobile for DL button - adds styling for mobile DL button

--- js/data_page.js
+++ js/data_page.js
@@ -1,11 +1,20 @@
 
-// Data.html DataTable Settings
+
 $(document).ready( function() {
+
+  var current_width = $(window).width();   
+// Data.html DataTable Settings
     $('#download_table').DataTable({
         "paging": false,
         "ordering": false,
         "info": false
     });
+
+//Disable Button Links on mobile devices
+  if (current_width <= 679) {
+    $('td.download_button a').attr('href', '');
+  }
+
 });
 
 // Initializes popovers
@@ -32,3 +41,5 @@ function copyToClipboard(elementId) {
   document.body.removeChild(aux);
 
 }
+
+
